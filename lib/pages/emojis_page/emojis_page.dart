ed0da44015bee1db29840c4ae4e8a7c40ef54f9b
Garrett Sangalli <gsharp.impact@gmail.com>
2021-11-21 14:18:27 -0600

    save

---
+++ lib/pages/emojis_page/emojis_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EmojisPage extends StatefulWidget {
+  const EmojisPage({Key? key}) : super(key: key);
+
+  @override
+  _EmojisPageState createState() => _EmojisPageState();
+}
+
+class _EmojisPageState extends State<EmojisPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
