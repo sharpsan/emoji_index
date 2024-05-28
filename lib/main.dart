78e79af84c6e323c8374212e1b9b0b45904d4470
Garrett <gsharp.impact@gmail.com>
2024-05-27 21:51:13 -0500

    play store assets

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Emoji Index',
       darkTheme: ThemeData.dark(),
       theme: ThemeData(
